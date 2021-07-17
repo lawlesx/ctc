d4b4e78e45983e6c22d57b7f88617e0afd0b533a
lawlesx <welcometohellv@gmail.com>
2021-07-17 23:14:57 +0530

    Added Loading effects and animation

--- components/ctable.tsx
+++ components/ctable.tsx
@@ -1,5 +1,5 @@
 import styles from '../styles/ctable.module.css';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export interface CtableProps {
   data: any;
@@ -27,63 +27,76 @@ const itemVariants = {
     scale: 1.1
   }
 }
+
+const errorVariants = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1
+  },
+  exit : {
+    opacity: 0,
+    scale: 2
+  }
+}
  
 const Ctable: React.FC<CtableProps> = ({data , coin}) => {
 
-  if(data!=undefined)
-    return ( 
-      <motion.div className={styles.container}>
-        <motion.div variants={topVariants} initial='hidden' animate='visible' className={styles.top}>
-          <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
-            <h3>Holders</h3>
-            <p>{(data.market_data.market_cap[coin]/data.market_data.current_price[coin]).toFixed(0)}</p>
-          </motion.div>
-          <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
-            <h3>Market Cap</h3>
-            <p>{data.market_data.market_cap[coin]}</p>
-          </motion.div>
-          <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
-            <h3>24H Volume</h3>
-            <p>{(data.market_data.market_cap_change_24h_in_currency[coin]).toFixed(5)}</p>
-          </motion.div>
-          <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
-            <h3>All-time-High</h3>
-            <p>{data.market_data.ath[coin]}</p>
-          </motion.div>
-          <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
-            <h3>All-time-Low</h3>
-            <p>{data.market_data.atl[coin]}</p>
-          </motion.div>
-          <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
-            <h3>Contract Address</h3>
-            <p className={styles.contract_address}>{data.contract_address}</p>
-          </motion.div>
-          <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
-            <h3>Full Diluted MC</h3>
-            <p>{data.market_data.fully_diluted_valuation[coin]}</p>
-          </motion.div>
-          <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
-            <h3>Current Supply</h3>
-            <p>{data.market_data.circulating_supply}</p>
-          </motion.div>
-          <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
-            <h3>Total Supply</h3>
-            <p>{data.market_data.total_supply}</p>
-          </motion.div>
+  return ( 
+    data!=undefined ?
+    <motion.div className={styles.container}>
+      <motion.div variants={topVariants} initial='hidden' animate='visible' className={styles.top}>
+        <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
+          <h3>Holders</h3>
+          <p>{(data.market_data.market_cap[coin]/data.market_data.current_price[coin]).toFixed(0)}</p>
         </motion.div>
-        <motion.div variants={topVariants} initial='hidden' animate='visible' className={styles.bot}>
-          <h3>Weekly Increase/Decrease</h3>
-          <p>{data.market_data.price_change_percentage_7d_in_currency[coin]}</p>
+        <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
+          <h3>Market Cap</h3>
+          <p>{data.market_data.market_cap[coin]}</p>
+        </motion.div>
+        <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
+          <h3>24H Volume</h3>
+          <p>{(data.market_data.market_cap_change_24h_in_currency[coin]).toFixed(5)}</p>
+        </motion.div>
+        <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
+          <h3>All-time-High</h3>
+          <p>{data.market_data.ath[coin]}</p>
+        </motion.div>
+        <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
+          <h3>All-time-Low</h3>
+          <p>{data.market_data.atl[coin]}</p>
+        </motion.div>
+        <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
+          <h3>Contract Address</h3>
+          <p className={styles.contract_address}>{data.contract_address}</p>
+        </motion.div>
+        <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
+          <h3>Full Diluted MC</h3>
+          <p>{data.market_data.fully_diluted_valuation[coin]}</p>
+        </motion.div>
+        <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
+          <h3>Current Supply</h3>
+          <p>{data.market_data.circulating_supply}</p>
+        </motion.div>
+        <motion.div variants={itemVariants} whileHover='hover' className={styles.item}>
+          <h3>Total Supply</h3>
+          <p>{data.market_data.total_supply}</p>
         </motion.div>
       </motion.div>
-    );
-  else {
-    return (
-      <div  className={styles.container}>
-        <h1>Loading</h1>
-      </div>
-    )
-  }
+      <motion.div variants={topVariants} initial='hidden' animate='visible' className={styles.bot}>
+        <h3>Weekly Increase/Decrease</h3>
+        <p>{data.market_data.price_change_percentage_7d_in_currency[coin]}</p>
+      </motion.div>
+    </motion.div>
+    :
+    <AnimatePresence>
+      <motion.div variants={errorVariants} initial='hidden' animate='visible' exit='exit' className={styles.error}>
+        <h1>Loading...</h1>
+      </motion.div>
+    </AnimatePresence>
+  );
+
 }
  
 export default Ctable;
